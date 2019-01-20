70df7844fb6d254e9bd10f0733abec13ef8a58f3
chamikab <kasun.chamika@gmail.com>
2019-01-20 22:48:17 +0530

    Added changes for understanding this

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-console.log('Chamika');
+function foo () {
+  console.log("Simple function call");
+  console.log(this === global);
+}
+
+foo();	//prints true on console
+console.log(this === global) //Prints true on console.
