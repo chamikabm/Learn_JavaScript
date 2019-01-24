4569501eadd00ed39e496d5dcc6fd2bb6da9c3c5
chamikab <kasun.chamika@gmail.com>
2019-01-24 22:34:33 +0530

    Added changes for understanding lexical bindings.

--- src/lexicalbinding.js
+++ src/lexicalbinding.js
@@ -16,3 +16,22 @@ const user = {
     alert(hello + langs)
   }
 };
+
+const user = {
+  name: 'Tyler',
+  age: 27,
+  languages: ['JavaScript', 'Ruby', 'Python'],
+  greet() {
+    const hello = `Hello, my name is ${this.name} and I know`
+
+    const langs = this.languages.reduce(function (str, lang, i) {
+      if (i === this.languages.length - 1) {
+        return `${str} and ${lang}.`
+      }
+
+      return `${str} ${lang},`
+    }.bind(this), "")
+
+    alert(hello + langs)
+  }
+}
