718865340bce0721cd5f7b923a9192e57beccb0a
chamikab <kasun.chamika@gmail.com>
2019-01-25 23:08:50 +0530

    Added changes for understanding async js.

---
+++ src/async.js
@@ -0,0 +1,8 @@
+let promise = loadScript("https://cdnjs.cloudflare.com/ajax/libs/lodash.js/3.2.0/lodash.js");
+
+promise.then(
+  script => alert(`${script.src} is loaded!`),
+  error => alert(`Error: ${error.message}`)
+);
+
+promise.then(script => alert('One more handler to do something else!'));
