b65e57e242553f277b72ab3a132dd9bcf34adf4c
chamikab <kasun.chamika@gmail.com>
2019-01-28 20:23:41 +0530

    Added changes for understanding itertorzs.

--- src/iterator.js
+++ src/iterator.js
@@ -26,4 +26,13 @@ while (!result.done) {
 }
 
 console.log("Iterated over sequence of size: ", result.value); // 5
-I
+
+
+function* makeRangeIterator(start = 0, end = Infinity, step = 1) {
+  let iterationCount = 0;
+  for (let i = start; i < end; i += step) {
+    iterationCount++;
+    yield i;
+  }
+  return iterationCount;
+}
